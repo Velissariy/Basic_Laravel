ffff0c3df007b8527c1bc2408fb1bf2102012ac3
Riddle <johnys2000@list.ru>
2024-11-14 22:59:13 +0500

    added books

--- code/laravel-project/app/Http/Controllers/UserController.php
+++ code/laravel-project/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -14,9 +15,10 @@ class UserController extends Controller {
 
     public function store(Request $request) {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'name' => 'required|string|max:50|min:3',
+            'surname' => 'required|string|max:50|min:3',
+            'email' => 'required|email|max:100|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         // Создание нового пользователя
@@ -30,7 +32,6 @@ class UserController extends Controller {
         // Аутентификация пользователя
         Auth::login($user);
 
-        // Редирект на главную страницу
         return redirect()->route('home');
     }
 
@@ -42,8 +43,8 @@ class UserController extends Controller {
 
     public function login(Request $request) {
         $request->validate([
-            'email' => 'required|string|max:255',
-            'password' => 'required|min:6',
+            'email' => 'required|string|max:100|min:3',
+            'password' => 'required|min:6|max:255',
         ]);
 
         // Попытка аутентификации
@@ -60,5 +61,90 @@ class UserController extends Controller {
     public function logout() {
         Auth::logout();
         return redirect()->route('home');
+    }
+
+    public function profile(?int $id = null) {
+        if (is_null($id)) {
+            $user = Auth::user();
+        } else {
+            $user = User::findOrFail($id);
+        }
+        return view('profile', compact('user'));
+    }
+
+    public function editProfile(?int $id = null) {
+        if (is_null($id)) {
+            $user = Auth::user();
+        } else {
+            $user = User::findOrFail($id);
+        }
+        return view('editProfile', compact('user'));
+    }
+
+    public function update(Request $request, $id) {
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:50|min:3',
+            'surname' => 'required|string|max:50|min:3',
+            'email' => 'required|email|max:100|unique:users,email,' . $user->id, // уникальность, исключая текущего пользователя
+        ]);
+
+        $user->update([
+            'first_name' => $request->input('name'),
+            'last_name' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password') ? Hash::make($request->input('password')) : $user->password,
+        ]);
+
+        return redirect()->route('home');
+    }
+
+    public function apiUsers() {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function apiUser(int $id) {
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    public function apiStore(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'surname' => 'required|string|max:50',
+            'email' => 'required|email|max:100|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'first_name' => $request->input('name'),
+            'last_name' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json($user, 201); // 201 - статус успешного создания
+    }
+
+    public function apiUpdate(Request $request, int $id) {
+        $user = User::findOrFail($id); // Находим пользователя по ID
+
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'surname' => 'required|string|max:50',
+            'email' => 'required|email|max:100|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        $user->update([
+            'first_name' => $request->input('name'),
+            'last_name' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password') ? Hash::make($request->input('password')) : $user->password, // Если пароль не указан, оставляем старый
+        ]);
+
+        return response()->json($user, 200); // 200 - статус успешного обновления
     }
 }
--- code/laravel-project/routes/web.php
+++ code/laravel-project/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\LogRequestController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfGeneratorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('main');
+})->name('home');
+
+Route::get('/users', [UserController::class, 'showList'])->name('users');
+
+Route::get('/register', [PageController::class, 'registration'])->name('register');
+
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+
+Route::get('/contacts', [PageController::class, 'contacts'])->name('contacts');
+
+Route::get('/login', [PageController::class, 'login'])->name('login');
+Route::post('/login', [UserController::class, 'login'])->name('user.login');
+
+Route::get('/logout', [UserController::class, 'logout'])->name('user.logout');
+
+Route::get('/user/{id}', [UserController::class, 'profile'])->name('user.profile');
+Route::get('/user/edit/{id}', [UserController::class, 'editProfile'])->name('edit.profile');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/{id}/download-pdf', [PdfGeneratorController::class, 'index'])->name('user.downloadPdf');
+
+Route::get('/api/users', [UserController::class, 'apiUsers'])->name('api.users');
+Route::get('/api/user/{id}', [UserController::class, 'apiUser'])->name('api.user');
+Route::put('/api/user/{id}', [UserController::class, 'apiUpdate'])->name('api.user.update');
+Route::post('/api/user', [UserController::class, 'apiStore'])->name('api.user.store');
+
+Route::get('/log-request', [LogRequestController::class, 'index'])->name('log-request');
